b8e08b4f926e1d9f68eeed0788aaeecce0ecc787
Jacson Rodrigues <github.obsessive123@passmail.net>
2026-07-24 21:34:17 -0300

    Uncomments user input sections

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades
-// utilizando scanf para entrada de dados e printf para exibir as informações.
-
 int main()
 {
     // Cidade A
@@ -20,21 +15,21 @@ int main()
     float super_powerA;
 
     // Coletando dados para a primeira carta
-    // printf("### First Card ###\n");
-    // printf("Enter the state (single character): ");
-    // scanf(" %c", &stateA);
-    // printf("Enter the state code (2 characters): ");
-    // scanf(" %2s", codeA);
-    // printf("Enter the city name: ");
-    // scanf(" %19s", cityA);
-    // printf("Enter the population: ");
-    // scanf(" %d", &populationA);
-    // printf("Enter the area (in square kilometers): ");
-    // scanf(" %f", &areaA);
-    // printf("Enter the GDP (in billions): ");
-    // scanf(" %f", &gdpA);
-    // printf("Enter the number of touristic spots: ");
-    // scanf(" %d", &touristic_spotsA);
+    printf("### First Card ###\n");
+    printf("Enter the state (single character): ");
+    scanf(" %c", &stateA);
+    printf("Enter the state code (2 characters): ");
+    scanf(" %2s", codeA);
+    printf("Enter the city name: ");
+    scanf(" %19s", cityA);
+    printf("Enter the population: ");
+    scanf(" %d", &populationA);
+    printf("Enter the area (in square kilometers): ");
+    scanf(" %f", &areaA);
+    printf("Enter the GDP (in billions): ");
+    scanf(" %f", &gdpA);
+    printf("Enter the number of touristic spots: ");
+    scanf(" %d", &touristic_spotsA);
 
     pop_densityA = (float) populationA / areaA;
     gdp_per_capitaA = (float) (((double) gdpA * 1e9) / populationA);
@@ -67,21 +62,21 @@ int main()
     float super_powerB;
 
     // Coletando dados para a segunda carta
-    // printf("### Second Card ###\n");
-    // printf("Enter the state (single character): ");
-    // scanf(" %c", &stateB);
-    // printf("Enter the state code (2 characters): ");
-    // scanf(" %2s", codeB);
-    // printf("Enter the city name: ");
-    // scanf(" %19s", cityB);
-    // printf("Enter the population: ");
-    // scanf(" %d", &populationB);
-    // printf("Enter the area (in square kilometers): ");
-    // scanf(" %f", &areaB);
-    // printf("Enter the GDP (in billions): ");
-    // scanf(" %f", &gdpB);
-    // printf("Enter the number of touristic spots: ");
-    // scanf(" %d", &touristic_spotsB);
+    printf("### Second Card ###\n");
+    printf("Enter the state (single character): ");
+    scanf(" %c", &stateB);
+    printf("Enter the state code (2 characters): ");
+    scanf(" %2s", codeB);
+    printf("Enter the city name: ");
+    scanf(" %19s", cityB);
+    printf("Enter the population: ");
+    scanf(" %d", &populationB);
+    printf("Enter the area (in square kilometers): ");
+    scanf(" %f", &areaB);
+    printf("Enter the GDP (in billions): ");
+    scanf(" %f", &gdpB);
+    printf("Enter the number of touristic spots: ");
+    scanf(" %d", &touristic_spotsB);
 
     pop_densityB = (float) populationB / areaB;
     gdp_per_capitaB = (float) (((double) gdpB * 1e9) / populationB);
@@ -184,13 +179,6 @@ int main()
         totalB += gdp_per_capitaB;
         break;
 
-        // case 7:
-        //   // Comparando Super Power
-        //   printf(
-        //       "In Super Power: %s City Wins\n",
-        //       super_powerA > super_powerB ? cityA : cityB);
-        //   break;
-
     default:
         printf("Invalid option!");
         break;
@@ -243,13 +231,6 @@ int main()
         totalA += gdp_per_capitaA;
         totalB += gdp_per_capitaB;
         break;
-
-        // case 7:
-        //   // Comparando Super Power
-        //   printf(
-        //       "In Super Power: %s City Wins\n",
-        //       super_powerA > super_powerB ? cityA : cityB);
-        //   break;
 
     default:
         printf("Invalid option!");
